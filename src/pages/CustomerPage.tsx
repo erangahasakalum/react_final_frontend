146ed219f10915ed7d8481233c1f22a7cc184150
Eranga Hasakalum <erangahasakalum6761@gmail.com>
2025-02-23 23:00:47 +0530

    add and design component in item page

--- src/pages/CustomerPage.tsx
+++ src/pages/CustomerPage.tsx
@@ -1,9 +1,9 @@
-import {Button, Form, Modal} from "antd";
+import {Button, Col, Form, Input, Modal, Row, Table} from "antd";
 import {useState} from "react";
 
 function CustomerPage() {
     const [title] = useState("Add Customer");
-    const [open, setOpen] = useState(true);
+    const [open, setOpen] = useState(false);
     const [confirmLoading, setConfirmLoading] = useState(false);
 
     const openAddModal = () => {
@@ -89,11 +89,11 @@ function CustomerPage() {
  */
     return (
         <div className="p-6">
-            <div>
+            <div className="flex justify-end">
                 <Button
                     type="primary"
                     onClick={openAddModal}
-                    className="position: relative left-1.5"
+                    className="bg-green-500 text-white p-2 rounded mr-2"
                 >
                     Add Customer
                 </Button>
@@ -104,10 +104,94 @@ function CustomerPage() {
                     confirmLoading={confirmLoading}
                     onCancel={handleCancel}
                 >
-                    <Form layout="vertical" requiredMark={false}>
+                    <Form requiredMark={false} layout={"vertical"}>
+                        <Row gutter={16}>
+                            <Col span={12}>
+                                <Form.Item
+                                    label="Id"
+                                    name="id"
+                                    rules={[
+                                        { required: true, message: "Please enter the id" },
+                                    ]}
+                                >
+                                    <Input
+                                        placeholder="Enter id"/>
 
+                                </Form.Item>
+                            </Col>
+                            <Col span={12}>
+                                <Form.Item
+                                    label="First Name"
+                                    name="firstName"
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: "Please enter the first name!",
+                                        },
+                                    ]}
+                                >
+                                    <Input
+                                        placeholder="Enter first name"
+
+                                    />
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                        <Row gutter={16}>
+                            <Col span={12}>
+                                <Form.Item
+                                    label="Last Name"
+                                    name="lastName"
+                                    rules={[
+                                        { required: true, message: "Please enter the last name!" },
+                                    ]}
+                                >
+                                    <Input
+                                        placeholder="Enter last name"/>
+
+                                </Form.Item>
+                            </Col>
+                            <Col span={12}>
+                                <Form.Item
+                                    label="Address"
+                                    name="address"
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: "Please enter the address!",
+                                        },
+                                    ]}
+                                >
+                                    <Input
+                                        placeholder="Enter address "
+
+                                    />
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                        <Row gutter={16}>
+                            <Col span={12}>
+                                <Form.Item
+                                    label="Email"
+                                    name="email"
+                                    rules={[
+                                        { required: true, message: "Please enter the email!" },
+                                    ]}
+                                >
+                                    <Input
+                                        placeholder="Enter email"/>
+
+                                </Form.Item>
+                            </Col>
+
+                        </Row>
                     </Form>
+
                 </Modal>
+            </div>
+
+            <div>
+                <Table/>
             </div>
         </div>
     )
--- src/pages/ItemPage.tsx
+++ src/pages/ItemPage.tsx
@@ -1,169 +1,185 @@
-import {useEffect, useState} from "react"
+/*import {useEffect, useState} from "react"
 import { Trash2 } from "react-feather"
 import {useDispatch, useSelector} from "react-redux";
 import {AppDispatch} from "../store/store.tsx";
 import {deleteItem, getALlItem, saveItem, updateItem} from "../reducer/ItemReducer.ts";
-import {Item} from "../models/Item.ts";
+import {Item} from "../models/Item.ts";*/
+import {Button, Col, Form, Input, Modal, Row, Table} from "antd";
+import {useState} from "react";
 
 function ItemPage() {
 
-  const [itemId, setItemId] = useState("")
-  const [name, setName] = useState("")
-  const [quantity, setQuantity] = useState("")
-  const [price, setPrice] = useState("")
-  const [isEditing, setIsEditing] = useState(false)
-  const dispatch = useDispatch<AppDispatch>();
-  const items = useSelector((state) => state.item || []);
+  const [title] = useState("Add Item");
+  const [open, setOpen] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
 
-  useEffect(() => {
-      getAllItems();
-  });
+  const openAddModal = () => {
+    setOpen(true);
+  };
 
-  const getAllItems = async () => {
-      await dispatch(getALlItem());
-  }
+  const handleOk = () => {
+    setConfirmLoading(true);
+    // Simulate a request or some action here
+    setTimeout(() => {
+      setOpen(false);
+      setConfirmLoading(false);
+    }, 1000);
+  };
 
-  const handleAdd = () => {
-    if (!itemId || !name || !quantity || !price) {
-      alert("All fields are required!")
-      return
-    }
-    const newItem = new Item(0,itemId,name,parseInt(quantity),parseInt(price));
-    dispatch(saveItem(newItem));
-    resetForm()
-  }
+  const handleCancel = () => {
+    setOpen(false);
+  };
 
-  const handleEdit = (item: any) => {
-    setItemId(item.itemId)
-    setName(item.name)
-    setQuantity(item.quantity)
-    setPrice(item.price)
-    setIsEditing(true)
-  }
 
-  const handleUpdate = () => {
-    if (!itemId || !name || !quantity || !price) {
-      alert("All fields are required!")
-      return
-    }
-    const updateCustomerDetails = new Item(0,itemId,name,parseInt(quantity),parseInt(price));
-    dispatch(updateItem(updateCustomerDetails));
-    resetForm()
-  }
+  /* const [itemId, setItemId] = useState("")
+   const [name, setName] = useState("")
+   const [quantity, setQuantity] = useState("")
+   const [price, setPrice] = useState("")
+   const [isEditing, setIsEditing] = useState(false)
+   const dispatch = useDispatch<AppDispatch>();
+   const items = useSelector((state) => state.item || []);
 
-  const handleDelete = (itemId: string) => {
-    if (window.confirm("Are you sure you want to delete this item?")) {
-        dispatch(deleteItem(itemId));
-    }
-  }
+   useEffect(() => {
+       getAllItems();
+   });
 
-  const resetForm = () => {
-    setItemId("")
-    setName("")
-    setQuantity("")
-    setPrice("")
-    setIsEditing(false)
-  }
+   const getAllItems = async () => {
+       await dispatch(getALlItem());
+   }
+
+   const handleAdd = () => {
+     if (!itemId || !name || !quantity || !price) {
+       alert("All fields are required!")
+       return
+     }
+     const newItem = new Item(0,itemId,name,parseInt(quantity),parseInt(price));
+     dispatch(saveItem(newItem));
+     resetForm()
+   }
+
+   const handleEdit = (item: any) => {
+     setItemId(item.itemId)
+     setName(item.name)
+     setQuantity(item.quantity)
+     setPrice(item.price)
+     setIsEditing(true)
+   }
+
+   const handleUpdate = () => {
+     if (!itemId || !name || !quantity || !price) {
+       alert("All fields are required!")
+       return
+     }
+     const updateCustomerDetails = new Item(0,itemId,name,parseInt(quantity),parseInt(price));
+     dispatch(updateItem(updateCustomerDetails));
+     resetForm()
+   }
+
+   const handleDelete = (itemId: string) => {
+     if (window.confirm("Are you sure you want to delete this item?")) {
+         dispatch(deleteItem(itemId));
+     }
+   }
+
+   const resetForm = () => {
+     setItemId("")
+     setName("")
+     setQuantity("")
+     setPrice("")
+     setIsEditing(false)
+   }*/
 
   return (
-    <div className="p-6">
-      <div className="grid grid-cols-2 gap-4 mb-4">
-        <input
-          type="text"
-          name="item_id"
-          placeholder="ItemPage ID"
-          value={itemId}
-          onChange={(e) => setItemId(e.target.value)}
-          className="border p-2 rounded"
-        />
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="border p-2 rounded"
-        />
-        <input
-          type="number"
-          name="quantity"
-          placeholder="Quantity"
-          value={quantity}
-          onChange={(e) => setQuantity(e.target.value)}
-          className="border p-2 rounded"
-        />
-        <input
-          type="number"
-          step="0.01"
-          name="price"
-          placeholder="Price"
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-          className="border p-2 rounded"
-        />
+      <div className="p-6">
+        <div className="flex justify-end">
+          <Button
+              type="primary"
+              onClick={openAddModal}
+              className="bg-green-500 text-white p-2 rounded mr-2"
+          >
+            Add Item
+          </Button>
+          <Modal
+              title={title}
+              open={open}
+              onOk={handleOk}
+              confirmLoading={confirmLoading}
+              onCancel={handleCancel}
+          >
+            <Form requiredMark={false} layout={"vertical"}>
+              <Row gutter={16}>
+                <Col span={12}>
+                  <Form.Item
+                      label="Id"
+                      name="id"
+                      rules={[
+                        { required: true, message: "Please enter the id" },
+                      ]}
+                  >
+                    <Input
+                        placeholder="Enter id"/>
+
+                  </Form.Item>
+                </Col>
+                <Col span={12}>
+                  <Form.Item
+                      label="Item Name"
+                      name="itemName"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please enter the item name!",
+                        },
+                      ]}
+                  >
+                    <Input
+                        placeholder="Enter item name"
+
+                    />
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Row gutter={16}>
+                <Col span={12}>
+                  <Form.Item
+                      label="Quantity"
+                      name="quantity"
+                      rules={[
+                        { required: true, message: "Please enter the quantity!" },
+                      ]}
+                  >
+                    <Input
+                        placeholder="Enter quantity"/>
+
+                  </Form.Item>
+                </Col>
+                <Col span={12}>
+                  <Form.Item
+                      label="Price"
+                      name="price"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please enter the price!",
+                        },
+                      ]}
+                  >
+                    <Input
+                        placeholder="Enter price "
+
+                    />
+                  </Form.Item>
+                </Col>
+              </Row>
+            </Form>
+
+          </Modal>
+        </div>
+
+        <div>
+          <Table/>
+        </div>
       </div>
-      <div className="flex justify-end">
-        {isEditing ? (
-          <button
-            onClick={handleUpdate}
-            className="bg-blue-500 text-white p-2 rounded mr-2"
-          >
-            Update
-          </button>
-        ) : (
-          <button
-            onClick={handleAdd}
-            className="bg-green-500 text-white p-2 rounded mr-2"
-          >
-            Add
-          </button>
-        )}
-        {isEditing && (
-          <button
-            onClick={resetForm}
-            className="bg-gray-500 text-white p-2 rounded"
-          >
-            Cancel
-          </button>
-        )}
-      </div>
-      <table className="min-w-full table-auto border-collapse mt-6">
-        <thead>
-          <tr className="bg-gray-100">
-            <th className="border px-4 py-2">ItemPage ID</th>
-            <th className="border px-4 py-2">Name</th>
-            <th className="border px-4 py-2">Quantity</th>
-            <th className="border px-4 py-2">Price</th>
-            <th className="border px-4 py-2">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {items.map((item: Item) => (
-            <tr
-              key={item.itemId}
-              onClick={() => handleEdit(item)}
-              className="hover:cursor-pointer hover:bg-slate-600 hover:text-white"
-            >
-              <td className="border px-4 py-2">{item.itemId}</td>
-              <td className="border px-4 py-2">{item.name}</td>
-              <td className="border px-4 py-2">{item.quantity}</td>
-              <td className="border px-4 py-2">{item.price}</td>
-              <td className="border px-4 py-2 text-center">
-                <button
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    handleDelete(item.itemId);
-                  }}
-                  className="bg-red-500 text-white p-2 rounded-lg"
-                >
-                  <Trash2 />
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
   )
 }
 
